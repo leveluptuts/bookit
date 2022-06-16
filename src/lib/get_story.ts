954dfb31d6204649ea1099140b1307d72b3f169c
Scott Tolinski <scott@leveluptuts.com>
2022-06-16 16:55:07 -0600

    adds sass as a dep

--- src/lib/get_story.ts
+++ src/lib/get_story.ts
@@ -2,15 +2,20 @@ import { get } from 'svelte/store';
 import { bookit_state } from './state';
 
 // Takes parent and title and plucks the correct
-// story from the tree
+// story from the treecd
 export async function getStory({ parent, title }) {
 	const tree = get(bookit_state).tree;
 	const group = tree?.[parent];
-	const found_story = group.find((item) => item.title === title);
-	const raw = await import(found_story.id + '?raw');
-	const raw_default = raw.default;
-	found_story.raw = raw_default;
-	return found_story;
+
+	if (group) {
+		const found_story = group.find((item) => item.title === title);
+		const raw = await import(found_story.id + '?raw');
+		const raw_default = raw.default;
+		found_story.raw = raw_default;
+		return found_story;
+	} else {
+		return null;
+	}
 }
 
 export function load({ params }) {
